e45edd25950d8717203dbdaa72f4edfa81cfde50
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-07-04 20:50:36 +0900

    feat: add button to navigate to thread root in nevent1 page

--- src/routes/(app)/(textnote)/nevent1{$id}.tsx
+++ src/routes/(app)/(textnote)/nevent1{$id}.tsx
@@ -1,6 +1,9 @@
+import { Link } from '@tanstack/react-router'
+import { MessageSquareReply } from 'lucide-react'
 import TextNote from '@/components/text-note/TextNote'
 import { TextNoteQuery } from '@/lib/nostr/kinds/1'
 import { createEvent } from '@/lib/nostr/nip19'
+import { Button } from '@/shadcn-ui/components/ui/button'
 
 export const Route = createFileRoute({
   component: RouteComponent,
@@ -24,8 +27,30 @@ export const Route = createFileRoute({
 
 function RouteComponent() {
   const { event } = Route.useLoaderData()
+  const hasRoot = (event.tags.find((tag) => {
+    return tag[0] === 'e' && tag[3] === 'root'
+  }) ?? [])[1] as string | undefined
   return (
     <>
+      {hasRoot && (() => {
+        const nevent = createEvent({
+          id: hasRoot,
+        })
+        return (
+          <Button className="mb-4" asChild variant="outline">
+            <Link
+              params={{
+                id: nevent.routeId,
+              }}
+              to="/nevent1{$id}"
+            >
+              <MessageSquareReply />
+              Go to thread root
+            </Link>
+          </Button>
+
+        )
+      })()}
       <TextNote event={event} whithReplies={true} />
     </>
   )
