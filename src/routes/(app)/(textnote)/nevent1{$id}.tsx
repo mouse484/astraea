c50128b5f1f447e8bc3bb25c4e2aab8c14662cd6
mouse484 <38714187+mouse484@users.noreply.github.com>
2025-07-04 15:17:19 +0900

    feat: nevent1 page display replies

--- src/routes/(app)/(textnote)/nevent1{$id}.tsx
+++ src/routes/(app)/(textnote)/nevent1{$id}.tsx
@@ -1,5 +1,6 @@
+import { Schema } from 'effect'
 import TextNote from '@/components/text-note/TextNote'
-import { TextNoteQuery } from '@/lib/nostr/kinds/1'
+import { TextNoteEventSchema, TextNoteQuery } from '@/lib/nostr/kinds/1'
 import { createEvent } from '@/lib/nostr/nip19'
 
 export const Route = createFileRoute({
@@ -7,18 +8,47 @@ export const Route = createFileRoute({
   loader: async ({ params: { id }, context: { queryClient, pool, relays } }) => {
     const nevent = createEvent(`nevent1${id}`)
     const decodedRelays = nevent.decoded.relays
-    const result = await queryClient.ensureQueryData(TextNoteQuery({
+    const event = await queryClient.ensureQueryData(TextNoteQuery({
       pool,
       relays: decodedRelays && decodedRelays.length > 0 ? decodedRelays : relays.read,
     }, nevent.decoded.id))
 
-    return result
+    if (!event) {
+      throw new Error(`Event with ID ${id} not found`)
+    }
+
+    const cacheReplies = await queryClient
+      .getQueryCache()
+      .findAll({ queryKey: ['reply', event.id] })
+    const replies = cacheReplies.map((item) => {
+      return Schema.decodeUnknownSync(TextNoteEventSchema)(item.state.data)
+    }).sort((a, b) => {
+      return (b.created_at ?? 0) - (a.created_at ?? 0)
+    })
+
+    return {
+      event,
+      replies,
+    }
   },
 })
 
 function RouteComponent() {
-  const event = Route.useLoaderData()
+  const { event, replies } = Route.useLoaderData()
   return (
-    <TextNote event={event} />
+    <>
+      <TextNote event={event} />
+      {
+        replies.length > 0 && (
+          <div className="border-l-4 pl-4">
+            {
+              replies.map(reply => (
+                <TextNote key={reply.id} event={reply} />
+              ))
+            }
+          </div>
+        )
+      }
+    </>
   )
 }
