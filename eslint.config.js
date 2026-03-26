d9a8241ee8d458db652366da047443ef176e9b6f
mouse484 <38714187+mouse484@users.noreply.github.com>
2026-03-26 22:38:16 +0900

    chore: enhance eslint configuration with TypeScript support and additional rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
-import { mouse } from '@mouse_484/eslint-config'
+import mouse, { GLOB_TSX } from '@mouse_484/eslint-config'
 
 export default mouse(
   {
+    typescript: {
+      tsconfigPath: './tsconfig.json',
+    },
     react: true,
     tailwind: {
       entryPoint: 'src/globals.css',
     },
     ignores: ['**/*.gen.ts', '**/src/shadcn-ui/**'],
+  },
+  {
+    files: [GLOB_TSX],
+    rules: {
+      'react-refresh/only-export-components': ['error', {
+        extraHOCs: ['createFileRoute'],
+      }],
+    },
   },
 )
