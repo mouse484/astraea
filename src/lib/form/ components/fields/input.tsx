ff1e976e17a7585b111f5ec2373105f4ba85ae36
mouse484 <38714187+mouse484@users.noreply.github.com>
2026-03-27 16:59:53 +0900

    fix: update RelayForm to use useAppForm

---
+++ src/lib/form/ components/fields/input.tsx
@@ -0,0 +1,25 @@
+import type { ComponentProps } from 'react'
+import type { BaseFieldProps } from './_base'
+import { Input } from '@/shadcn-ui/components/ui/input'
+import BaseField, { extractBaseFieldProps } from './_base'
+
+interface Props extends BaseFieldProps, ComponentProps<'input'> { }
+
+export default function InputField(properties: Props) {
+  const { baseFieldProps, restProps } = extractBaseFieldProps(properties)
+  return (
+    <BaseField<string> {...baseFieldProps}>
+      {(field, state) => (
+        <Input
+          id={field.name}
+          aria-invalid={state.isInvalid}
+          name={field.name}
+          value={field.state.value}
+          onBlur={field.handleBlur}
+          onChange={event => field.handleChange(event.target.value)}
+          {...restProps}
+        />
+      )}
+    </BaseField>
+  )
+}
