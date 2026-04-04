c72aab015e72eb3143e704201803e2a829434c0e
mouse484 <38714187+mouse484@users.noreply.github.com>
2026-04-04 13:37:29 +0900

    fix: @vitejs/plugin-react-swc to @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
